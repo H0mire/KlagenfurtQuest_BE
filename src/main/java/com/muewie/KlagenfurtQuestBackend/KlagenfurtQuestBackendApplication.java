a57669b803c1e54714eaebff0d4f71c80da8e699
H0mire <janikmueller12@gmail.com>
2024-01-26 13:42:18 +0100

    test

---
+++ src/main/java/com/muewie/KlagenfurtQuestBackend/KlagenfurtQuestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.muewie.KlagenfurtQuestBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlagenfurtQuestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlagenfurtQuestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muewie/KlagenfurtQuestBackend/KlagenfurtQuestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muewie.KlagenfurtQuestBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlagenfurtQuestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
